0497ef5e025e2242d54e1dd4430e704e4acee8a2
Sabesh <sabesh@consciousengines.com>
2026-04-21 20:53:38 +0530

    Initial Commit

---
+++ modelhub/modelhubApp.swift
@@ -0,0 +1,17 @@
+//
+//  modelhubApp.swift
+//  modelhub
+//
+//  Created by Sabesh Bharathi on 21/04/26.
+//
+
+import SwiftUI
+
+@main
+struct modelhubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
